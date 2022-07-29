14faf778afcf809e65c8ea770fcaa9b3b7828c3c
sebastianbarreram <88840800+sebastianbarreram@users.noreply.github.com>
2022-07-29 14:06:13 -0500

    Deploy firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
 	production: true,
+	firebaseConfig: {
+		apiKey: "AIzaSyCKbKsFdn7fU2VrkwsNkitfsayXlqhWwKw",
+		authDomain: "retofinalaplicacionempresarial.firebaseapp.com",
+		projectId: "retofinalaplicacionempresarial",
+		storageBucket: "retofinalaplicacionempresarial.appspot.com",
+		messagingSenderId: "545642575624",
+		appId: "1:545642575624:web:d7c8ec1b4059182a9f9ebd",
+	}
 };
